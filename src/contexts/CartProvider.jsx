5618ccf17956003583c7dd2c1947049df87c1c80
Facundo Varela <facundovarela88@hotmail.com>
2026-03-06 21:38:55 -0300

    Re commit

---
+++ src/contexts/CartProvider.jsx
@@ -0,0 +1,60 @@
+import {CartContext} from './cartContext.jsx'
+import { useState, useEffect } from 'react'
+
+export function CartProvider({children}){
+    const [cart, SetCart] = useState(() => {
+        const storedCart = localStorage.getItem('cart');
+        return storedCart ? JSON.parse(storedCart) : [];
+    });
+
+    // Persist cart in localStorage whenever it changes
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
+
+    const addToCart = (producto) =>{
+        console.log('Producto en addToCart: ', producto)
+        const IsInCart = cart.some(prod => prod.id === producto.id);
+        if (!IsInCart) {
+            SetCart([...cart, producto])
+        } else {
+            const updatedCart = cart.map(item => {
+                if (item.id === producto.id) return {...item, quantity: item.quantity + producto.quantity}
+                return item
+            })
+            SetCart(updatedCart)
+        }
+    }
+
+    const substractFromCart = (producto) => {
+        const updatedCart = cart
+            .map(item => {
+                if (item.id === producto.id) {
+                    return { ...item, quantity: item.quantity - 1 };
+                }
+                return item;
+            })
+            .filter(item => item.quantity > 0);
+        SetCart(updatedCart);
+    }
+
+    const removeFromCart = (productoId) => {
+        const updatedCart = cart.filter(prod => prod.id !== productoId);
+        SetCart(updatedCart);
+    }
+
+    const GetTotalProducts = () =>{
+        const totalQuantity = cart.reduce((acc, current) => acc + current.quantity, 0);
+        return totalQuantity
+    }
+
+    const GetCart = () =>{
+        return cart;
+    }
+
+    return(
+        <CartContext.Provider value={{addToCart, GetTotalProducts, GetCart, removeFromCart, substractFromCart}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CartProvider } from './contexts/CartProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
